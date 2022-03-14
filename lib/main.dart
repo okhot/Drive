8ec541cea2c92270afb796a6304830520b5d4ec1
okhot <arreyetta1jnr@gmail.com>
2022-03-14 13:04:39 +0100

    commit on drive

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -50,6 +51,61 @@ class LoginScreen extends StatelessWidget {
             "Login to Take a ride",
             style: TextStyle(
                 color: Colors.black, fontSize: 45, fontWeight: FontWeight.bold),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                hintText: "User Email",
+                prefix: Icon(
+                  Icons.mail,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 26,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextField(
+              obscureText: true,
+              keyboardType: TextInputType.visiblePassword,
+              decoration: InputDecoration(
+                hintText: "User Password",
+                prefix: Icon(
+                  Icons.lock,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+            child: Text(
+              "Forgot Password",
+              style: TextStyle(color: Colors.blue),
+            ),
+          ),
+          SizedBox(
+            height: 88,
+          ),
+          Container(
+            width: double.infinity,
+            child: RawMaterialButton(
+              fillColor: Colors.blue,
+              elevation: 0.0,
+              padding: EdgeInsets.symmetric(vertical: 20.0),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+              onPressed: () {},
+              child: Text("Login"),
+            ),
           )
         ],
       ),
